c5af39d0283038609e278494cca96cadc70b948f
schachi5000 <alexander.schacher@graphmasters.net>
2023-10-20 14:05:22 +0200

    :tada: project setup

---
+++ android/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "NUNAV Sdk Example"
+include(":app")
+ 
